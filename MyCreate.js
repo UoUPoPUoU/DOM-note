377785467656439ff0503eeb1221232ce78e0951
Yoyo Yin <825086234@qq.com>
2017-10-31 11:03:39 +0800

    Update MyCreate.js

--- MyCreate.js
+++ MyCreate.js
@@ -49,33 +49,63 @@ function getNextElement(node){
  }
 
 //封装：移动图片，动画效果
-function moveElement(elementID,final_x,final_y,interval) {
-    var elem = document.getElementById(elementID);
-    var xpos = parseInt(elem.style.left);
-    var ypos = parseInt(elem.style.top);
-    var dist = 0;
-    clearTimeout(elem.movement);
-    if (xpos == final_x && ypos == final_y) {
-        return true;
+ function prepareSlideshow() {
+        var Oas = document.getElementById("nav").getElementsByTagName("a");
+        var pic = document.getElementById("pic");
+        pic.style.position = "absolute";
+        Oas[0].onmouseover = function () {
+            moveElement("pic", 0, 0, 10);
+        };
+        Oas[1].onmouseover = function () {
+            moveElement("pic", -100, 0, 10);
+        };
+        Oas[2].onmouseover = function () {
+            moveElement("pic", -200, 0, 10);
+        };
     }
-    if (xpos < final_x) {
-        xpos = xpos + (Math.ceil((final_x - xpos)/10));
-    }
-    if (xpos > final_x) {
-        xpos = xpos - (Math.ceil((xpos - final_x)/10));
-    }
-    if (ypos < final_y) {
-        ypos = ypos + (Math.ceil((final_y - ypos)/10));
-    }
-    if (ypos > final_y) {
-        ypos = ypos - (Math.ceil((ypos - final_y)/10));
-    }
-    elem.style.left = xpos + "px";
-    elem.style.top = ypos + "px";
-    var repeat ="moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
-    elem.movement = setTimeout(repeat,interval);
-}
 
+    function moveElement(elementID, final_x, final_y, interval) {
+        if (!document.getElementById) return false;
+        if (!document.getElementById(elementID)) return false;
+        var elem = document.getElementById(elementID);
+        var dist;
+        if (elem.movement) {
+            clearTimeout(elem.movement);
+        }
+        if (!elem.style.left) {
+            elem.style.left = "0px";
+        }
+        if (!elem.style.top) {
+            elem.style.top = "0px";
+        }
+        var xpos = parseInt(elem.style.left);
+        var ypos = parseInt(elem.style.top);
+        if (xpos === final_x && ypos === final_y) {
+            return true;
+        }
+        if (xpos < final_x) {
+            dist = Math.ceil((final_x - xpos) / 10);
+            xpos = xpos + dist;
+        }
+        if (xpos > final_x) {
+            dist = Math.ceil((xpos - final_x) / 10);
+            xpos = xpos - dist;
+        }
+        if (ypos < final_y) {
+            dist = Math.ceil((final_y - ypos) / 10);
+            ypos = ypos + dist;
+        }
+        if (ypos > final_y) {
+            dist = Math.ceil((ypos - final_y) / 10);
+            ypos = ypos - dist;
+        }
+        elem.style.left = xpos + "px";
+        elem.style.top = ypos + "px";
+        var repeat = moveElement(elementID, final_x, final_y, interval);
+        elem.movement = setTimeout(repeat, interval);
+    }
+
+   
 // 图片灰色被触碰变彩色
 function  convertTOGS(img) {
     // 如果没有canvas 就退出
